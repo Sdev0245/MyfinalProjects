289258a6be6480e4e26cbfdd46f8c54e522f7605
nerdninja <sdeven515@gmail.com>
2018-04-06 22:10:12 +0530

    Create readme.m

---
+++ Devim Text editor/readme.m
@@ -0,0 +1,7 @@
+### DEVIM TEXT EDITOR
+
+#### This is the text editor that i am trying to build in c language.It would be able to run on linux and windows.Currently  its not
+complete.But you can actually compile it by simply 
+
+gcc Devim.c
+and ./a.out to run
